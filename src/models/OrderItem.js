8b19c5e26ca3adf613aebebf02eb5133860912e9
Kim Da Eun <midekuna@gmail.com>
2023-11-14 17:24:20 +0900

    refactor: OrderItem에 category 필드 추가

--- src/models/OrderItem.js
+++ src/models/OrderItem.js
@@ -1,17 +1,20 @@
-import { CATEGORIES, MENUS } from '../constants/menus.js';
 import MESSAGES from '../constants/messages.js';
+import { CATEGORIES, MENUS } from '../constants/menus.js';
 
 import { getValueOfField } from '../utils/object.js';
 import throwError from '../utils/throwError.js';
 import { isMenuExists } from '../utils/validators.js';
 
 class OrderItem {
+  #category;
+
   #menu;
 
   #amount;
 
   constructor(menu, amount) {
     this.#validate(menu);
+    this.#setCategory(menu);
     this.#menu = menu;
     this.#amount = amount;
   }
@@ -20,26 +23,23 @@ class OrderItem {
     if (!isMenuExists(menu)) throwError(MESSAGES.errors.invalidMenu);
   }
 
-  get totalPrice() {
-    const price = getValueOfField(
-      MENUS,
-      `${this.#getCategory()}.${this.#menu}`,
+  #setCategory(menu) {
+    this.#category = CATEGORIES.find((category) =>
+      getValueOfField(MENUS, `${category}.${menu}`),
     );
+  }
+
+  get totalPrice() {
+    const price = getValueOfField(MENUS, `${this.#category}.${this.#menu}`);
     return price * this.#amount;
   }
 
   get item() {
     return {
-      category: this.#getCategory(),
+      category: this.#category,
       menu: this.#menu,
       amount: this.#amount,
     };
-  }
-
-  #getCategory() {
-    return CATEGORIES.find((category) =>
-      getValueOfField(MENUS, `${category}.${this.#menu}`),
-    );
   }
 }
 
